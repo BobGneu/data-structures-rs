3c8dcc3400d3f8619f73059bf63db6a98c95ca0d
Bob Chatman <bob.chatman@gmail.com>
2018-02-27 00:30:10 -0800

    Adding in dealing with raw arrays.

--- src/sort.rs
+++ src/sort.rs
@@ -4,6 +4,38 @@ pub fn swap(array: &mut Vec<i32>,from: usize,to: usize) {
     array[to] = tmp;
 }
 
+pub fn swap_arr(array: &mut [i32],from: usize,to: usize) { 
+    let tmp = array[from];
+    array[from] = array[to];
+    array[to] = tmp;
+}
+
+pub fn bubble_sort_arr(array: &mut [i32]) {
+    let mut walker: usize = 0;
+    let mut number_of_sorted_elements: usize = 0;
+
+    loop {
+        if number_of_sorted_elements >= (array.len() - 1) {
+            break;
+        }
+
+        loop {
+            if walker >= (array.len() - number_of_sorted_elements - 1) {
+                break;
+            }
+
+            if array[walker] > array[walker + 1] {
+                swap_arr(array, walker, walker + 1);
+            }
+
+            walker += 1;
+        }
+
+        walker = 0;
+        number_of_sorted_elements += 1;
+    }
+}
+
 pub fn bubble_sort(array: &mut Vec<i32>) {
     let mut walker: usize = 0;
     let mut number_of_sorted_elements: usize = 0;
@@ -54,6 +86,39 @@ pub fn selection_sort(array: &mut Vec<i32>) {
 
         if smallest != number_of_sorted_elements {
             swap(array, smallest, number_of_sorted_elements);
+        }
+
+        number_of_sorted_elements += 1;
+        
+        walker = number_of_sorted_elements;
+        smallest = walker;
+    }
+}
+
+pub fn selection_sort_arr(array: &mut [i32]) {
+    let mut number_of_sorted_elements: usize = 0;
+    let mut smallest: usize = 0;
+    let mut walker: usize = number_of_sorted_elements + 1;
+
+    loop {
+        if number_of_sorted_elements == (array.len() - 1) {
+            break;
+        }
+
+        loop {
+            if walker == array.len() {
+                break;
+            }
+
+            if  array[walker] < array[smallest] {
+                smallest = walker
+            }
+
+            walker += 1
+        }
+
+        if smallest != number_of_sorted_elements {
+            swap_arr(array, smallest, number_of_sorted_elements);
         }
 
         number_of_sorted_elements += 1;
--- tests/sorting.rs
+++ tests/sorting.rs
@@ -15,6 +15,15 @@ test_suite! {
         assert_eq!(array_input, array_sorted);
     }
 
+    test bubble_sort_arr_1() {
+        let mut array_input = [10,3,5,2,1,6,4,9,8,7];
+        let array_sorted = [1,2,3,4,5,6,7,8,9,10];
+
+        bubble_sort_arr(&mut array_input);
+
+        assert_eq!(array_input, array_sorted);
+    }
+
     test bubble_sort_2() {
         let mut array_input = [96,	21,	29,	52,	54,10,	55,	66,	59,	18,58,	46,	31,	12,	40,53].to_vec();
         let array_sorted = [10, 12, 18, 21, 29, 31, 40, 46, 52, 53, 54, 55, 58, 59, 66, 96].to_vec();
@@ -24,11 +33,29 @@ test_suite! {
         assert_eq!(array_input, array_sorted);
     }
 
+    test bubble_sort_arr_2() {
+        let mut array_input = [96,	21,	29,	52,	54,10,	55,	66,	59,	18,58,	46,	31,	12,	40,53];
+        let array_sorted = [10, 12, 18, 21, 29, 31, 40, 46, 52, 53, 54, 55, 58, 59, 66, 96];
+
+        bubble_sort_arr(&mut array_input);
+
+        assert_eq!(array_input, array_sorted);
+    }
+
     test bubble_sort_3() {
         let mut array_input = [100, 25, 65, 95, 24, -87, 14, 49, 99, 36, 8, 84, 43, 32, 93, 19, 21, 86, -4, 74].to_vec();
         let array_sorted = [-87, -4, 8, 14, 19, 21, 24, 25, 32, 36, 43, 49, 65, 74, 84, 86, 93, 95, 99, 100].to_vec();
 
         bubble_sort(&mut array_input);
+
+        assert_eq!(array_input, array_sorted);
+    }
+
+    test bubble_sort_arr_3() {
+        let mut array_input = [100, 25, 65, 95, 24, -87, 14, 49, 99, 36, 8, 84, 43, 32, 93, 19, 21, 86, -4, 74];
+        let array_sorted = [-87, -4, 8, 14, 19, 21, 24, 25, 32, 36, 43, 49, 65, 74, 84, 86, 93, 95, 99, 100];
+
+        bubble_sort_arr(&mut array_input);
 
         assert_eq!(array_input, array_sorted);
     }
@@ -47,6 +74,15 @@ test_suite! {
         assert_eq!(array_input, array_sorted);
     }
 
+    test selection_sort_arr_1() {
+        let mut array_input = [10,3,5,2,1,6,4,9,8,7];
+        let array_sorted = [1,2,3,4,5,6,7,8,9,10];
+    
+        selection_sort_arr(&mut array_input);
+
+        assert_eq!(array_input, array_sorted);
+    }
+
     test selection_sort_2() {
         let mut array_input = [96,	21,	29,	52,	54,10,	55,	66,	59,	18,58,	46,	31,	12,	40,53].to_vec();
         let array_sorted = [10, 12, 18, 21, 29, 31, 40, 46, 52, 53, 54, 55, 58, 59, 66, 96].to_vec();
@@ -56,11 +92,29 @@ test_suite! {
         assert_eq!(array_input, array_sorted);
     }
 
+    test selection_sort_arr_2() {
+        let mut array_input = [96,	21,	29,	52,	54,10,	55,	66,	59,	18,58,	46,	31,	12,	40,53];
+        let array_sorted = [10, 12, 18, 21, 29, 31, 40, 46, 52, 53, 54, 55, 58, 59, 66, 96];
+
+        selection_sort_arr(&mut array_input);
+
+        assert_eq!(array_input, array_sorted);
+    }
+
     test selection_sort_3() {
         let mut array_input = [100, 25, 65, 95, 24, -87, 14, 49, 99, 36, 8, 84, 43, 32, 93, 19, 21, 86, -4, 74].to_vec();
         let array_sorted = [-87, -4, 8, 14, 19, 21, 24, 25, 32, 36, 43, 49, 65, 74, 84, 86, 93, 95, 99, 100].to_vec();
 
         selection_sort(&mut array_input);
+
+        assert_eq!(array_input, array_sorted);
+    }
+
+    test selection_sort_arr_3() {
+        let mut array_input = [100, 25, 65, 95, 24, -87, 14, 49, 99, 36, 8, 84, 43, 32, 93, 19, 21, 86, -4, 74];
+        let array_sorted = [-87, -4, 8, 14, 19, 21, 24, 25, 32, 36, 43, 49, 65, 74, 84, 86, 93, 95, 99, 100];
+
+        selection_sort_arr(&mut array_input);
 
         assert_eq!(array_input, array_sorted);
     }
